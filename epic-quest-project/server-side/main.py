1426d1a863585961e6adf2bc3246a687c8761cbf
KellenJobh <kelen175@hotmail.com>
2023-09-01 12:53:05 +0000

    Pub/Sub Handler

--- epic-quest-project/server-side/main.py
+++ epic-quest-project/server-side/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Epic Quest Project / CICD pipeline + PubSub'
+    return 'Hello, Epic Quest Project / CICD pipeline + PubSub jq'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
