2d1bcf6377d6fa9970beeb869bc977a9692c2ef8
KellenJobh <kelen175@hotmail.com>
2024-02-14 03:07:04 +0000

    add doc string

--- finops/list-static-ip-cloud-function.py
+++ finops/list-static-ip-cloud-function.py
@@ -2,21 +2,31 @@ import os
 from google.cloud import compute_v1
 
 project = os.environ.get('GCP_PROJECT')
- 
+
 def list_unused_static_ips(request):
+    """
+    檢視並返回所有未使用的靜態 IP 地址。
+
+    Args:
+        request (flask.Request): 進入 Cloud Function 的請求物件。
+
+    Returns:
+        dict: 包含未使用的靜態 IP 地址資訊的字典，格式為 {'unused_static_ips': unused_ips}。
+    """
+
     # 從環境變數中獲取要檢查的所有地區
     regions = get_all_regions()
- 
+
     # 初始化 Compute 客戶端
     compute_client = compute_v1.AddressesClient()
- 
+
     unused_ips = []
- 
+
     # 遍歷所有地區
     for region in regions:
-        # 檢索指定地區的IP地址        
+        # 檢索指定地區的 IP 地址
         addresses = compute_client.list(project=project, region=region)
- 
+
         # 遍歷地區中的 IP 地址
         for address in addresses:
             # 檢索每個 IP 地址的屬性
@@ -25,19 +35,25 @@ def list_unused_static_ips(request):
                 'type': address.address_type,
                 'region': region
             }
- 
+
             # 如果地址未使用，則將其資訊添加到列表中
             if address.status == 'RESERVED':
                 unused_ips.append(address_details)
- 
+
     # 返回所有未使用的靜態 IP 地址資訊
-    print(unused_ips)
-    return {'unused__ips(External&Internal IP)': unused_ips}
- 
+    return {'unused_static_ips': unused_ips}
+
 def get_all_regions():
-    # 初始化 Regions Client
+    """
+    檢索並返回所有可用地區的名稱。
+
+    Returns:
+        list: 包含所有可用地區名稱的列表。
+    """
+
+    # 初始化 Regions 客戶端
     region_client = compute_v1.RegionsClient()
- 
+
     # 獲取所有地區的名稱
     regions = []
     for region in region_client.list(project=project):
